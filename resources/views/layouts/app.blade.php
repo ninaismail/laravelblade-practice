28dd9d21e61eb864c8e748f9b95cb36ded9e3755
Najat Ismail <najatt.ismail@gmail.com>
2023-07-14 16:01:39 +0300

    fixed something

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <title>Laravel 8 Form Example Tutorial</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>

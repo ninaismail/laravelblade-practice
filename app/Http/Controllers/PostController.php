f811dbc50e168e4300971cf3e2e1f1fc1c953a9b
Najat Ismail <najatt.ismail@gmail.com>
2023-07-14 09:20:54 +0300

    insitial commits

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('add-blog-post-form');
+    }
+    public function store(Request $request)
+    {
+        $post = new Post;
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->save();
+        return redirect('add-blog-post-form')->with('status', 'Blog Post Form Data Has Been inserted');
+    }
+}

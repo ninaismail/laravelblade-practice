c24ba476ee1d87a1de2333d9fd732ce45b43b977
Najat Ismail <najatt.ismail@gmail.com>
2023-07-15 09:12:28 +0300

    used partials

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,7 @@
 <!-- app.blade.php -->
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel 8 Form Example Tutorial</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
+@include('partials._head')
 <body>
+    @include('partials._nav')
     <div class="container">
         <div class="row">
             <div class="col-md-8 offset-md-2">
@@ -15,5 +9,6 @@
             </div>
         </div>
     </div>
+    @include('partials._footer')
 </body>
 </html>
---
+++ resources/views/partials/_head.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 8 Form Example Tutorial</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
